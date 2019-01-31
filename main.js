29405b607c7ffc64c623aa8ed6f5df084999d552
Lukholo Mini <lukholo.mini@hotmail.com>
2019-01-31 06:41:49 +0200

    add animation to image gallery

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ imgs.forEach(img => img.addEventListener('click', imageClick));
 function imageClick(e) {
   imgs.forEach(img => (img.style.opacity = 1));
   current.src = e.target.src;
+  current.classList.add('fade-in');
+
+  // remove fade in class after
+  setTimeout(() => current.classList.remove('fade-in'), 500);
 
   e.target.style.opacity = opacity;
 }
